963bdcce5d2450be792f83fd8028b67567efdcd6
edadlani <40873038+edadlani@users.noreply.github.com>
2018-07-05 15:51:55 -0700

    System --> SSH
    test script for system --> SSH

---
+++ IPF.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+test1=C1
+test2=C2
+echo "Which system do you want to ssh into?"
+echo "Options: C1, C2"
+read system_name
+if [ $system_name == $test1 ];then
+        echo "Login Successful."
+	ssh root@192.168.1.102
+elif [ $system_name == $test2 ];then
+        echo "Login Successful"
+	ssh root@192.168.1.100
+else
+	echo "No System Found."
+        echo "Login Failed."
+fi
+
